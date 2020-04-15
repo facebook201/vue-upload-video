dffd337952d069d884cf8e0e88c05e33534f4ad8
syo <syolmm@qq.com>
2020-04-15 18:12:42 +0800

    xhr概念实现流程

---
+++ document.md
@@ -0,0 +1,18 @@
+## 实现
+
+
+
+
+
+### XMLHttpRequest
+
+
+
+XHR2 来实现上传，它支持二级制文件、配合 FormData对象管理表单、配合进度提示、可以通过 xhr.uplad一些事件回调来获取传输进度。设置 timeout 和 ontimeout 超时。
+
+XMLHttpRequest   ===>   XMLHttpRequestEventTarget  ===>  EventTarget
+
+**如果对于全双工的通信， WebSocket 是更好的选择。**
+
+
+
---
+++ src/package/upload-video/src/index.vue
@@ -0,0 +1,36 @@
+<script>
+
+
+export default {
+  props: {
+    type: String,
+  },
+
+  render(h) {
+    let {
+
+    },
+
+    const data = {
+      class: {
+        'dm-upload-video': true,
+      },
+      on: {
+        click: handleClick,
+      }
+    };
+
+    data.class[`dm-upload-video`] = true;
+
+    return (
+      <div {...data} tabindex="0">
+        {
+          // 是否有其他功能比如拖拽
+          this.$slots.default
+        }
+        <input class="dm-upload-video" type="file" ref="input" on-change={handleChange} multiple={multiple} accept={accept}></input>
+      </div>
+    );
+  }
+};
+</script>
